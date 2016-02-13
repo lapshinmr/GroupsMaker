2033fc375eb28650efaed7c2c45480baa341beb0
Mikhail Lapshin <lapshinmr@gmail.com>
2016-02-13 18:27:12 +0300

    += combine

--- groups_maker.py
+++ groups_maker.py
@@ -4,15 +4,28 @@ You can find main logic here.
 
 
 class GroupsMaker:
-    def __init__(self, student_names, total_lessons):
+    def __init__(self, student_names, total_lessons, group_amount=2):
         self.student_names = student_names
         self.total_lessons = total_lessons
+        self.group_amount = group_amount
 
     def combine(self):
-        print(self.student_names)
+        combs = [(name, ) for name in self.student_names]
+        while len(combs[0]) < self.group_amount:
+            output = []
+            for name in self.student_names:
+                for comb in combs:
+                    if name not in comb:
+                        comb += (name,)
+                        output.append(comb)
+            else:
+                combs = output[:]
+        unique_combs = set([tuple(sorted(comb)) for comb in combs])
+        return unique_combs
+        # output = set([tuple(sorted(comb)) for comb in combs])
 
 if __name__ == '__main__':
-    students = ['misha', 'kate', 'yula']
-    g = GroupsMaker(students, 6)
-    g.combine()
+    students = ['misha', 'kate', 'yula', 'serega', 'dasha', 'sasha']
+    g = GroupsMaker(students, 5, group_amount=4)
+    print(g.combine())
 
