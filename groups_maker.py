b450b7b5f936c81d56d52c9a87283ddbf00357aa
Mikhail Lapshin <lapshinmr@gmail.com>
2016-02-13 15:30:08 +0300

    my first project commit

---
+++ groups_maker.py
@@ -0,0 +1,18 @@
+"""
+You can find main logic here.
+"""
+
+
+class GroupsMaker:
+    def __init__(self, student_names, total_lessons):
+        self.student_names = student_names
+        self.total_lessons = total_lessons
+
+    def combine(self):
+        print(self.student_names)
+
+if __name__ == '__main__':
+    students = ['misha', 'kate', 'yula']
+    g = GroupsMaker(students, 3)
+    g.combine()
+
