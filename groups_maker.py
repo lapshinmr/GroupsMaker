416dae0d87e5509c4ae65187438379514deccc54
Mikhail Lapshin <lapshinmr@gmail.com>
2016-02-13 19:13:56 +0300

    += get_random_comb; get_remain_names; get_calendar

--- groups_maker.py
+++ groups_maker.py
@@ -1,31 +1,53 @@
 """
 You can find main logic here.
 """
+import random
 
 
 class GroupsMaker:
-    def __init__(self, student_names, total_lessons, group_amount=2):
+    def __init__(self, student_names, total_lessons, group_amount=2, unwanted_combs=[]):
         self.student_names = student_names
         self.total_lessons = total_lessons
         self.group_amount = group_amount
+        self.unique_combs = []
+        self.used_combs = []
 
     def combine(self):
         combs = [(name, ) for name in self.student_names]
         while len(combs[0]) < self.group_amount:
-            output = []
+            tmp_combs = []
             for name in self.student_names:
                 for comb in combs:
                     if name not in comb:
                         comb += (name,)
-                        output.append(comb)
+                        tmp_combs.append(comb)
             else:
-                combs = output[:]
-        unique_combs = set([tuple(sorted(comb)) for comb in combs])
-        return unique_combs
-        # output = set([tuple(sorted(comb)) for comb in combs])
+                combs = tmp_combs[:]
+        self.unique_combs = set([tuple(sorted(comb)) for comb in combs])
+
+    def get_random_comb(self, name):
+        combs_with_name = []
+        for comb in self.unique_combs:
+            if name in comb:
+                combs_with_name.append(comb)
+        return random.choice(combs_with_name)
+
+    def get_remain_names(self, names, comb):
+        comb = list(comb)
+        for name in names:
+            comb.remove(name)
+        return tuple(comb)
+
+    def get_calendar(self, unique_combs):
+        while True:
+            name, *other_names = random.shuffle(self.student_names)
+            comb = self.get_random_comb(name)
+            self.used_combs.append(comb)
+
 
 if __name__ == '__main__':
-    students = ['misha', 'kate', 'yula', 'serega', 'dasha', 'sasha']
-    g = GroupsMaker(students, 5, group_amount=4)
-    print(g.combine())
+    students = ['misha', 'kate', 'serega', 'yula', 'dasha', 'sasha']
+    g = GroupsMaker(students, 2, group_amount=3)
+    g.combine()
+    print(g.get_remain_names(['misha'], g.get_random_comb('misha')))
 
