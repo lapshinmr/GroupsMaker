c3c5ca35a629d40914a22be3c3ab49e6bcff1efa
Mikhail Lapshin <lapshinmr@gmail.com>
2016-02-17 10:51:06 +0300

    add table by grid

--- interface_gui.py
+++ interface_gui.py
@@ -1,33 +1,16 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from groups_maker import GroupsMaker
 
 
-class StudentInfoButton(Button):
-    def __init__(self, parent=None, **config):
-        Button.__init__(self, parent, **config)
-        self.pack()
-        self.config(command=self.callback)
-
-    def callback(self):
-        print('hello')
-
-
-# def fetch():
-#     var = StringVar()
-#     var.set(ent.get())
-#     button_box = Frame(root)
-#     button_box.pack(side=LEFT)
-#     for name in var.get().split(','):
-#         name_button = StudentInfoButton(button_box, text=name.strip())
-#         name_button.pack(side=TOP)
-#         name_button.config(width=10)
-
 def get_names_from_string(names_string):
-    names_string = names_string.split(',')
-    return [name.strip() for name in names_string]
+    seps = '.,;'
+    for sep in seps:
+        names_string = names_string.replace(sep, ' ')
+    return names_string.split()
 
-def combine():
-    names = get_names_from_string(ent.get())
+
+def get_calendar():
     g = GroupsMaker(names, 3)
     calendar = g.get_calendar()
     with open('calendar', 'w') as f:
@@ -40,17 +23,45 @@ def combine():
             f.write('\n')
 
 
+def add():
+    names = get_names_from_string(ent.get())
+    label_box = Frame(root)
+    label_box.pack(side=LEFT)
+    for name in names:
+        name_label = Label(label_box, text=name)
+        name_label.pack(side=TOP)
+        name_label.config(width=10)
+
+
+# Main window
 root = Tk()
+root.title('GroupsMaker')
+root.resizable(width=FALSE, height=FALSE)
+#root.geometry('600x400')
+
 sub = Frame(root)
-sub.pack(side=TOP, expand=YES, fill=X)
-Label(sub, text='Entry students names or copy/paste it').pack(side=TOP, expand=YES, fill=X)
+sub.pack(side=TOP, fill=X)
+lab = Label(sub, text='Entry students names or copy/paste it')
+lab.pack(side=TOP, expand=YES, fill=X)
+
 ent = Entry(sub)
 ent.insert(0, 'misha, kate, yula, serega, dasha, sasha')
-ent.pack(side=TOP, expand=YES, fill=X)
+ent.pack(side=TOP, fill=X)
 ent.focus()
-ent.bind('<Return>', (lambda event: combine()))
-but = Button(sub, text='combine', command=(lambda:  combine()))
-but.pack(side=RIGHT)
+ent.bind('<Return>', (lambda event: add()))
+
+Button(sub, text='add', command=(lambda: add())).pack(side=LEFT)
+Button(sub, text='combine', command=(lambda: get_calendar())).pack(side=RIGHT)
+
+names = list(range(1, 46))
+tab = Frame(root)
+tab.pack(side=TOP, anchor=W)
+for num, name in enumerate(names):
+    lab = Label(tab, text=num + 1, relief=RIDGE, width=5)
+    ent = Entry(tab, width=20)
+    lab.grid(row=num % 15, column=((num // 15) * 2))
+    ent.grid(row=num % 15, column=((num // 15) * 2 + 1))
+    # ent.insert(0, name)
 
 
 root.mainloop()
