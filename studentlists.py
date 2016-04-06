4b9427715f569d1bbb52b59660b37ec61039ad9a
Mikhail Lapshin <lapshinmr@gmail.com>
2016-04-06 15:10:00 +0300

    working version of lists editor with three NamesBoxes

--- studentlists.py
+++ studentlists.py
@@ -3,15 +3,15 @@ from widgets import TipButton
 
 
 class NamesBox(Frame):
-    def __init__(self, parent, names):
+    def __init__(self, parent, names, consumer=None):
         Frame.__init__(self, parent)
         self.names = list(names)
-        self.other_names_box = None
         self.listbox = None
+        self.consumer = consumer
         self.show()
 
-    def connect(self, other_names_box):
-        self.other_names_box = other_names_box
+    def connect(self, consumer):
+        self.consumer = consumer
 
     def show(self):
         self.listbox = Listbox(self)
@@ -20,18 +20,13 @@ class NamesBox(Frame):
         sbar.pack(side=RIGHT, fill=Y)
         self.listbox.config(yscrollcommand=sbar.set)
         self.listbox.pack(side=LEFT, expand=YES, fill=BOTH)
-        self.listbox.bind('<Double-1>', self.pop)
-        self.listbox.bind('<Double-3>', self.pop)
+        self.listbox.bind('<Double-1>', lambda event: self.pop())
         self.fill()
 
     def fill(self):
         self.listbox.delete(0, END)
         for name in self.names:
             self.listbox.insert(END, name)
-
-    def add(self, name):
-        self.names.append(name)
-        self.fill()
 
     def pop(self):
         try:
@@ -42,27 +37,28 @@ class NamesBox(Frame):
         except TclError as e:
             print('Listbox is empty (%s)' % e.__class__.__name__)
         else:
-            return name
+            self.consumer.names.append(name)
+            self.consumer.fill()
 
 
 class ListsEditor(Frame):
     def __init__(self, parent=None, name='', names=(), whitelist=(), blacklist=()):
         Frame.__init__(self, parent)
         self.pack()
-        # INPUT DATA
         self.parent = parent
         self.name = name
-        self.names = names
+        self.names = names.remove(name) if name in names else names
         self.whitelist = whitelist
         self.blacklist = blacklist
-        if self.name in names:
-            names.remove(self.name)
-        # FRAMES
+        self.exclist_selector = BooleanVar()
+        self.exclist_selector.set(0)
+        self.main_listbox = None
+        self.white_listbox = None
+        self.black_listbox = None
+        self.listbox = None
         self.name_fr = None
         self.names_fr = None
         self.exclists_fr = None
-        self.listbox = None
-        # DRAW WIDGETS
         self.make_widgets()
 
     def make_widgets(self):
@@ -80,25 +76,38 @@ class ListsEditor(Frame):
     def show_names(self):
         names_fr = Frame(self)
         names_fr.pack(side=TOP, expand=YES, fill=BOTH)
-        self.names = NamesBox(names_fr, self.names)
-        self.names.pack(side=TOP, expand=YES, fill=BOTH)
+        self.main_listbox = NamesBox(names_fr, self.names)
+        self.main_listbox.pack(side=TOP, expand=YES, fill=BOTH)
 
     def show_lists_buttons(self):
         lists_buttons_fr = Frame(self)
         lists_buttons_fr.pack(side=TOP, expand=YES, fill=X)
-        Button(lists_buttons_fr, text='white', command=lambda: 0).pack(side=LEFT, expand=YES, fill=X)
-        Button(lists_buttons_fr, text='black', command=lambda: 0).pack(side=RIGHT, expand=YES, fill=X)
+        Radiobutton(
+            lists_buttons_fr, text='white', command=self.connect_to_exclist, variable=self.exclist_selector, value=0
+        ).pack(side=LEFT, expand=YES, fill=X)
+        Radiobutton(
+            lists_buttons_fr, text='black', command=self.connect_to_exclist, variable=self.exclist_selector, value=1
+        ).pack(side=RIGHT, expand=YES, fill=X)
 
     def show_exclists(self):
         exclists_fr = Frame(self)
         exclists_fr.pack(side=TOP, expand=YES, fill=BOTH)
-        NamesBox(exclists_fr, self.whitelist).pack(side=LEFT, expand=YES, fill=BOTH)
-        NamesBox(exclists_fr, self.blacklist).pack(side=LEFT, expand=YES, fill=BOTH)
+        self.white_listbox = NamesBox(exclists_fr, self.whitelist, self.main_listbox)
+        self.white_listbox.pack(side=LEFT, expand=YES, fill=BOTH)
+        self.main_listbox.connect(self.white_listbox)
+        self.black_listbox = NamesBox(exclists_fr, self.blacklist, self.main_listbox)
+        self.black_listbox.pack(side=LEFT, expand=YES, fill=BOTH)
 
     def show_accept(self):
         accept_fr = Frame(self)
         accept_fr.pack(side=BOTTOM, expand=YES, fill=X)
         Button(accept_fr, text='Accept', command=self.accept).pack(side=BOTTOM, expand=YES, fill=X)
+
+    def connect_to_exclist(self):
+        if self.exclist_selector.get():
+            self.main_listbox.connect(self.black_listbox)
+        else:
+            self.main_listbox.connect(self.white_listbox)
 
     def accept(self):
         if self.parent is None:
