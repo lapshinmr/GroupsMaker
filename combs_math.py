2b13a68064611c4b8b6accccb3cde7459436c7eb
Mikhail Lapshin <lapshinmr@gmail.com>
2016-04-20 14:58:42 +0300

    separate ttgen from combs_math

--- combs_math.py
+++ combs_math.py
@@ -1,5 +1,3 @@
-import random
-from gm_exceptions import *
 
 
 def unpack(combs_list):
@@ -94,91 +92,5 @@ def remove_combs_by_item(item, combs_list):
     return list(set(combs_list) - set(exclist))
 
 
-class TimetableGenerator:
-    def __init__(self, names, comb_size=2, lessons_total=1, whitelist=(), blacklist=(), repetitions=False):
-        self.names = names
-        self.comb_size = comb_size
-        self.repetitions = repetitions
-        self.lessons_total = lessons_total
-        if len(self.names) < comb_size:
-            raise NotEnoughStudents
-        self.whitelist = sort_items_in_all_combs(whitelist, dups=False)
-        self.blacklist = sort_items_in_all_combs(blacklist, dups=False)
-        self.uniq_combs = gen_sorted_combs(self.names, comb_size, uniq=True)
-        self.uniq_combs_outblack = subtract_combs(self.uniq_combs, self.blacklist)
-        self.uniq_combs_outblack = subtract_combs(self.uniq_combs, self.blacklist)
-        # self.limit_attempts = self.les_total * attempts_factor
-
-    def get_lesson(self, in_combs_list):
-        names = self.names[:]
-        combs_list = in_combs_list[:]
-        output_combs = []
-        remainder = len(names) % self.comb_size
-        while len(names) > remainder:
-            comb = combs_list.pop(0)
-            output_combs.append(comb)
-            for name in comb:
-                combs_list = remove_combs_by_item(name, combs_list)
-                names.remove(name)
-        for comb in output_combs:
-            in_combs_list.remove(comb)
-        for idx, item in enumerate(names):
-            output_combs[idx] += (item,)
-        return output_combs, in_combs_list
-
-    def get_lessons(self, in_combs_list):
-        combs = in_combs_list[:]
-        calendar = []
-        for attempt in range(self.lessons_total):
-            try:
-                lesson, combs = self.get_lesson(combs)
-            except IndexError:
-                return calendar
-            if lesson:
-                calendar.append(lesson)
-        return calendar
-
-    def get_les_versions(self, in_combs_list):
-        combs = in_combs_list[:]
-        versions = []
-        for dummy in range(1000):
-            random.shuffle(combs)
-            versions.append(self.get_lessons(combs))
-        return versions
-
-    """
-    def get_timetable(self):
-        timetable = []
-        parts = []
-        timetable.extend(self.get_part_without_whitelist())
-        if not self.repetitions:
-            return timetable, parts
-        while len(timetable) < self.les_total:
-            next_tt_part = self.get_lessons(self.uniq_combs)
-            if next_tt_part:
-                parts.append(len(timetable))
-                timetable.extend(next_tt_part)
-            self.attempts += 1
-            if self.attempts > self.limit_attempts:
-                raise NotEnoughStudents
-        else:
-            cur_ttlen = len(timetable)
-            extra_ttlen = cur_ttlen - self.les_total
-            if extra_ttlen:
-                timetable = timetable[:-extra_ttlen]
-        return timetable, parts
-
-    def get_attempts(self):
-        return self.attempts
-    """
-
-
 if __name__ == '__main__':
-    tt = TimetableGenerator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 2, 10)
-    count = {}
-    versions = tt.get_les_versions()
-    for vers in versions:
-        key = len(vers)
-        count[key] = count.get(key, []) + [vers]
-    for idx, key in enumerate(sorted(count.keys())):
-        print(idx + 1, len(count[key]))
+    pass
---
+++ unittests/ttgen_test.py
@@ -0,0 +1,72 @@
+import unittest
+from ttgen import *
+
+
+class TestTimetableGenerator(unittest.TestCase):
+    def test_init_tt(self):
+        init_error_case = [
+            ([], 2),
+            ([], 2),
+            ([], 2),
+            ([], 2),
+            ([], 3),
+            ([], 3),
+            ([], 3),
+            ([1, 2], 3),
+        ]
+        for items, comb_size in init_error_case:
+            with self.assertRaises(NotEnoughStudents):
+                TimetableGenerator(items, comb_size)
+
+    def test_get_lesson(self):
+        lesson_case = [
+            ([1, 2, 3], [(1, 2), (2, 3), (1, 3)], 2, ([(1, 2, 3)], [(2, 3), (1, 3)])),
+            ([1, 2, 3], [(1, 3), (2, 3), (1, 2)], 2, ([(1, 3, 2)], [(2, 3), (1, 2)])),
+            ([1, 2, 3, 4], [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)], 2, ([(1, 2), (3, 4)], [(1, 3), (1, 4), (2, 3), (2, 4)])),
+            ([1, 2, 3, 4], [(1, 4), (2, 3), (2, 4), (3, 4)], 2, ([(1, 4), (2, 3)], [(2, 4), (3, 4)])),
+            ([1, 2, 3, 4], [(2, 3), (1, 4), (2, 4), (3, 4)], 2, ([(2, 3), (1, 4)], [(2, 4), (3, 4)])),
+            ([1, 2, 3, 4], [(2, 3), (1, 4), (2, 4), (3, 4)], 2, ([(2, 3), (1, 4)], [(2, 4), (3, 4)])),
+        ]
+        for items, uniq_combs, comb_size, output in lesson_case:
+            self.assertEqual(TimetableGenerator(items, comb_size).get_lesson(uniq_combs), output)
+
+        lesson_error_case = [
+            ([1, 2, 3, 4], [(2, 3), (2, 4), (3, 4)], 2),
+            ([1, 2, 3, 4], [(3, 4), (2, 3), (1, 4), (2, 4)], 2)
+        ]
+        for items, uniq_combs, comb_size in lesson_error_case:
+            with self.assertRaises(IndexError):
+                TimetableGenerator(items, comb_size).get_lesson(uniq_combs)
+
+    def test_get_lessons(self):
+        get_lessons_case = [
+            ([1, 2], 1, 1, [[(1, ), (2, )]]),
+            ([1, 2], 1, 2, [[(1, 2)]]),
+            ([1, 2, 3], 1, 2, [[(1, 2, 3)]]),
+            ([1, 2, 3], 2, 2, [[(1, 2, 3)], [(1, 3, 2)]]),
+            ([1, 2, 3], 3, 2, [[(1, 2, 3)], [(1, 3, 2)], [(2, 3, 1)]]),
+            ([1, 2, 3, 4], 1, 2, [[(1, 2), (3, 4)]]),
+            ([1, 2, 3, 4], 2, 2, [[(1, 2), (3, 4)], [(1, 3), (2, 4)]]),
+            ([1, 2, 3, 4], 3, 2, [[(1, 2), (3, 4)], [(1, 3), (2, 4)], [(1, 4), (2, 3)]]),
+            ([1, 2, 3], 1, 3, [[(1, 2, 3)]]),
+            ([1, 2, 3, 4], 1, 3, [[(1, 2, 3, 4)]]),
+            ([1, 2, 3, 4], 2, 3, [[(1, 2, 3, 4)], [(1, 2, 4, 3)]]),
+            ([1, 2], 2, 2, [[(1, 2)]]),
+            ([1, 2, 3], 2, 3, [[(1, 2, 3)]]),
+            ([1, 2, 3], 3, 3, [[(1, 2, 3)]]),
+        ]
+        for items, lessons_total, comb_size, output in get_lessons_case:
+            tt = TimetableGenerator(items, comb_size, lessons_total)
+            self.assertEqual(tt.get_lessons(tt.uniq_combs), output)
+
+    def test_get_les_versions(self):
+        get_les_verdioins_case = [
+            ([1, 2], 2, 3, 1000),
+        ]
+        for items, comb_size, lessons_total, output in get_les_verdioins_case:
+            tt = TimetableGenerator(items, comb_size, lessons_total)
+            self.assertEqual(len(tt.get_les_versions()), output)
+
+
+if __name__ == '__main__':
+    unittest.main()
